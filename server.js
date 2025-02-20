3664bece5297850b4137d1ae258e2b9dfc18db2a
Your Name <your.email@example.com>
2025-02-20 19:09:12 +0100

    new changes

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.use(express.json()); // Allows the server to accept JSON data in requests
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
+    useNewUrlParser: true,  // Keep this option
 })
 .then(() => console.log('MongoDB Connected'))
 .catch(err => console.log('MongoDB Connection Error:', err));
