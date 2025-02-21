a52fe77a2748e3a09dc1bb351ef340a2e3b02f9d
Your Name <your.email@example.com>
2025-02-21 13:49:46 +0100

    new changes

--- server.js
+++ server.js
@@ -20,4 +20,8 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
+app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+  
 app.listen(5000, () => console.log("Server running on port 5000"));
